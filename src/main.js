ac726a4a814d0b6696416719763dd0759eecd68c
xinsuishang <xinsuishang@outlook.com>
2020-06-24 02:26:26 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
 
+//生产环境去除注释
 Vue.config.productionTip = false
+
+Vue.use(Router)
 
 new Vue({
   render: h => h(App),
